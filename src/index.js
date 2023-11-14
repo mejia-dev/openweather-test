2db2d1ab99dbde0cdb6c00ceb27afec47e76f156
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-14 10:42:16 -0800

    Split url format into a separate function. Format url based on user selection

--- src/index.js
+++ src/index.js
@@ -4,21 +4,55 @@ import './css/styles.css';
 
 // Business Logic
 
-function getWeather(input) {
-  let request = new XMLHttpRequest();
-  let url;
-  if (input.match(/[0-9]{5}/g)) {
-    url = `http://api.openweathermap.org/data/2.5/weather?zip=${input}&appid=${process.env.API_KEY}&units=imperial`;
+// function getWeather(input) {
+//   let request = new XMLHttpRequest();
+//   let url;
+//   if (input.match(/[0-9]{5}/g)) {
+//     url = `http://api.openweathermap.org/data/2.5/weather?zip=${input}&appid=${process.env.API_KEY}&units=imperial`;
+//   } else {
+//     url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${process.env.API_KEY}&units=imperial`;
+//   }
+
+//   request.addEventListener("loadend", function() {
+//     const response = JSON.parse(this.responseText);
+//     if (this.status === 200) {
+//       printElements(response, input);
+//     } else {
+//       printError(this, input);
+//     }
+//   });
+
+//   request.open("GET", url, true);
+//   request.send();
+// }
+
+function formatRequest(location,outputOption) {
+  let locationTypeUrl;
+  if (location.match(/[0-9]{5}/g)) {
+    locationTypeUrl = `http://api.openweathermap.org/data/2.5/REPLACEME?zip=${location}&appid=${process.env.API_KEY}&units=imperial`;
   } else {
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${process.env.API_KEY}&units=imperial`;
+    locationTypeUrl = `http://api.openweathermap.org/data/2.5/REPLACEME?q=${location}&appid=${process.env.API_KEY}&units=imperial`;
   }
 
+  let outputOptionUrl;
+  if (outputOption === "weather") {
+    outputOptionUrl = locationTypeUrl.replace("REPLACEME","weather");
+  } else if (outputOption === "forecast") {
+    outputOptionUrl = locationTypeUrl.replace("REPLACEME","forecast");
+  }
+
+  let url = outputOptionUrl;
+  getWeatherInfo(url,location);
+}
+
+function getWeatherInfo(url,location) {
+  let request = new XMLHttpRequest();
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      printElements(response, input);
+      printElements(response, location);
     } else {
-      printError(this, input);
+      printError(this, location);
     }
   });
 
@@ -37,21 +71,14 @@ function printError(request, city) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}:  ${request.status} ${request.statusText}`;
 }
 
-function handleTempFormSubmission(event) {
+function handleFormSubmission(event) {
   event.preventDefault();
   const location = document.querySelector('#location').value;
+  const outputOption = document.querySelector("input[name='outputOption']:checked").value;
   document.querySelector('#location').value = null;
-  getWeather(location);
-}
-
-function handleForecastFormSubmission(event) {
-  event.preventDefault();
-  const location = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getForecast(location);
+  formatRequest(location,outputOption);
 }
 
 window.addEventListener("load", function() {
-  document.getElementById('tempHumidButton').addEventListener("click", handleTempFormSubmission);
-  document.getElementById('fiveDayButton').addEventListener("click", handleForecastFormSubmission);
+  document.getElementById('formSubmit').addEventListener("click", handleFormSubmission);
 });
