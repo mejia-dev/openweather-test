2b9b0162eae2759ef0b92fbb32f376c69eeb0d03
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-14 08:46:15 -0800

    Setup initial project

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'OpenWeather API Test Project',
       template: './src/index.html',
       inject: 'body'
     })
